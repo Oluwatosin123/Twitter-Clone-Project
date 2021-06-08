206c946872e683425e01866737dff7398ce0ad13
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-08 08:14:37 +0100

    added login functionality and validation

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 //  DOM Elements
 const mainPage = document.querySelector('.main-page');
 const loginPage = document.querySelector('.login-page');
-const middleContent = document.querySelector('middle-content');
+const middleContent = document.querySelector('.middle-content');
+const btnTop = document.querySelector('.btn-top');
+const newsFeedPage = document.querySelector('.feeds-page')
 
 /*******************************/
 /*******************************/
 
 // Main Page
 const goToLoginPage = () => {
-    mainPage.style.display = 'grid';
-    loginPage.style.display = 'none';
+    mainPage.style.display = 'none';
+    loginPage.style.display = 'grid';
 };
 
 middleContent.addEventListener('click', e => {
-    if (e.target.classList[1] === 'main-btn') {
+    if (e.target.classList[0] === 'main-btn') {
         goToLoginPage()
     }
-})
+    console.log(e.target.classList);
+});
+
+btnTop.addEventListener('click', () => {
+    const inputUserInfo = document.querySelector('.user-info');
+    const inputPassword = document.querySelector('.password')
+
+
+    if (inputUserInfo.value !== "" && inputPassword.value !== "") {
+        mainPage.style.display = 'none';
+        newsFeedPage.style.display = 'block';
+    } else {
+        goToLoginPage();
+    }
+});
+
