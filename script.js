8543c8754d64492bd2307ac135580e39b30d24a9
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-08 09:13:49 +0100

    modal functionality done

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const mainPage = document.querySelector('.main-page');
 const loginPage = document.querySelector('.login-page');
 const middleContent = document.querySelector('.middle-content');
 const btnTop = document.querySelector('.btn-top');
-const newsFeedPage = document.querySelector('.feeds-page')
+const newsFeedPage = document.querySelector('.feeds-page');
+const loginModal = document.querySelector('.login-modal');
+const modalX = document.querySelector('.login-modal i')
 
 /*******************************/
 /*******************************/
 
 // Main Page
 const goToLoginPage = () => {
+    mainPage.style.display = 'none';
+    loginPage.style.display = 'grid';
 };
-mainPage.style.display = 'none';
-loginPage.style.display = 'grid';
+
 
 middleContent.addEventListener('click', e => {
     if (e.target.classList[0] === 'main-btn') {
@@ -31,6 +34,12 @@ btnTop.addEventListener('click', () => {
         newsFeedPage.style.display = 'block';
     } else {
         goToLoginPage();
+        loginModal.style.display = 'block'
     }
 });
+
+// Login page
+modalX.addEventListener('click', () => {
+    loginModal.style.display = 'none';
+})
 
