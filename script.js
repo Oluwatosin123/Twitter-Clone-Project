b8b84e06454d354571f85a6f4f30d4916b011edf
Ajose Tosin <tosinemidastouch@gmail.com>
2021-06-09 09:34:54 +0100

    building of sidebar

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ const middleContent = document.querySelector('.middle-content');
 const btnTop = document.querySelector('.btn-top');
 const newsFeedPage = document.querySelector('.feeds-page');
 const loginModal = document.querySelector('.login-modal');
-const modalX = document.querySelector('.login-modal i')
-
+const modalX = document.querySelector('.login-modal, .login-modal-icon');
+const loginFormBtn = document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalX = document.querySelector('.modal-header, modal-header-i ');
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
+const modalInput = document.querySelector('.modal-input');
 /*******************************/
 /*******************************/
 
@@ -21,7 +28,6 @@ middleContent.addEventListener('click', e => {
     if (e.target.classList[0] === 'main-btn') {
         goToLoginPage()
     }
-    console.log(e.target.classList);
 });
 
 btnTop.addEventListener('click', () => {
@@ -39,7 +45,54 @@ btnTop.addEventListener('click', () => {
 });
 
 // Login page
-modalX.addEventListener('click', () => {
-    loginModal.style.display = 'none';
-})
+    modalX.addEventListener('click', e => {
+         if(e.target.classList === "") ;
+        loginModal.style.display = 'none'
+    });
 
+    loginFormBtn.addEventListener('click', () => {
+        const loginUserInfo = document.querySelector('.login-user-info');
+        const loginPassword = document.querySelector('.login-password');
+
+        if (loginUserInfo.value !== "" && loginPassword !== "") {
+            loginPage.style.display = 'none';
+            newsFeedPage.style.display = 'block';
+        } else {
+            loginModal.style.display = 'block'
+        }
+    })
+
+
+// News feed modal
+// post modal
+postBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalWrapper.classList.add('modal-wrapper-display');
+});
+
+const changeOpacity = x => {
+    modalPostBtn.style.opacity = x;
+    modalFooterPlus.style.opacity = x;
+}
+
+postModalX.addEventListener('click', (e) => {
+    if (e.target.classList.value === "");modalWrapper.classList.remove('modal-wrapper-display');
+
+
+    if(modalInput.value !== "") {
+        modalInput.value === "";
+        changeOpacity(0.5);
+
+    }
+});
+
+modalInput.addEventListener('keypress',(e) => {
+    if (e.target.value !== "") {
+        changeOpacity(1);
+    }
+});
+
+modalInput.addEventListener('blur', (e) => {
+        if(e.target.value === "");
+        changeOpacity(0.5);
+})
